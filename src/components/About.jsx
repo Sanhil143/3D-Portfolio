621aeaa14da79c209c198649a52b8cfda07c9ad4
Sanhil143 <sanhilrai143@gmail.com>
2023-12-22 00:05:56 +0530

    update abiut

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import {motion} from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>About  </div>
   )
 }
 
